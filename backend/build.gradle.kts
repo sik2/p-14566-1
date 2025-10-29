045f2bcbe7515d8335fa674cd8046805ec08607d
sik2 <bickwang123@gmail.com>
2025-10-29 14:57:43 +0900

    deploy setting

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     // 벨리데이션
     implementation("org.springframework.boot:spring-boot-starter-validation")
@@ -46,6 +47,11 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
     // oauth2
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+
+    runtimeOnly("com.mysql:mysql-connector-j") // 추가됨
+    implementation("org.springframework.boot:spring-boot-starter-actuator") // 추가됨
+    implementation("org.springframework.boot:spring-boot-starter-data-redis") // 추가됨
+    implementation("org.springframework.session:spring-session-data-redis") // 추가됨
 }
 
 tasks.withType<Test> {
