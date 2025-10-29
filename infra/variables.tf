559ac2c415ec11aeee963d03bd9e85bebe4cafa4
sik2 <bickwang123@gmail.com>
2025-10-29 12:33:34 +0900

    infra init

---
+++ infra/variables.tf
@@ -0,0 +1,15 @@
+
+variable "region" {
+  description = "region"
+  default     = "ap-northeast-2"
+}
+
+variable "prefix" {
+  description = "Prefix for all resources"
+  default     = "terra"
+}
+
+variable "app_1_domain" {
+  description = "app_1 domain"
+  default     = "api.p-14566-1.sik2.site"
+}
